d01dea88f844d1d3c75d6a8d4b5ba521f4084e80
sinadavodi <117124411+sinadavodi@users.noreply.github.com>
2026-01-30 21:17:20 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.158/build/three.module.js";
 import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.158/examples/jsm/loaders/GLTFLoader.js";
 
-import { State } from "./state.js";
-import { Personality } from "./personality.js";
 import { initScene } from "./scene.js";
 import { initUI } from "./ui.js";
+import { State } from "./state.js";
+import { Personality } from "./personality.js";
 import { initMusic } from "./music.js";
 
-initScene(THREE, GLTFLoader, Personality);
+initScene(THREE, GLTFLoader, State, Personality);
 initUI(State, Personality);
 initMusic(Personality);
