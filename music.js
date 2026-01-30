bfa521a81f5cba08240a8e1af6838ab544bfcf53
sinadavodi <117124411+sinadavodi@users.noreply.github.com>
2026-01-30 21:03:41 +0330

    Delete music.js

--- music.js
+++
@@ -1,9 +0,0 @@
-export function initMusic(Personality){
-  const audio = new Audio(`music/${Personality.music}.mp3`);
-  audio.loop = true;
-  audio.volume = 0.5;
-
-  document.body.addEventListener("click",()=>{
-    audio.play().catch(()=>{});
-  },{ once:true });
-}
