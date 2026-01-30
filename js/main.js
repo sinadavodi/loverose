eb639f38bf324f22c82cb1a188b1094c10eebea2
sinadavodi <117124411+sinadavodi@users.noreply.github.com>
2026-01-30 21:33:53 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.158/build/three.module.js";
 import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.158/examples/jsm/loaders/GLTFLoader.js";
 
-import { initScene } from "./scene.js";
-import { initUI } from "./ui.js";
 import { State } from "./state.js";
 import { Personality } from "./personality.js";
-import { initMusic } from "./music.js";
+import { initScene } from "./scene.js";
+
+const hour = new Date().getHours();
+if (hour >= 22 || hour <= 5) {
+  document.getElementById("nightStyle").disabled = false;
+}
 
 initScene(THREE, GLTFLoader, State, Personality);
-initUI(State, Personality);
-initMusic(Personality);
