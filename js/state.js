877925b533412382e23e23fbff1b56b00060aa3e
sinadavodi <117124411+sinadavodi@users.noreply.github.com>
2026-01-30 21:34:52 +0330

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,19 +1,5 @@
-export const State = {};
-
-State.start = localStorage.start || (localStorage.start = Date.now());
-State.lastVisit = localStorage.last || Date.now();
-localStorage.last = Date.now();
-
-State.daysTogether =
-  Math.floor((Date.now()-State.start)/86400000);
-
-State.daysAway =
-  Math.floor((Date.now()-State.lastVisit)/86400000);
-
-// stages: 0 healthy | 1 lonely | 2 sad | 3 dying
-State.stage = Math.min(State.daysAway,3);
-
-// احیا
-State.heal = ()=>{
-  localStorage.last = Date.now();
+export const State = {
+  awake: false,
+  attention: 100,
+  lastVisit: Date.now()
 };
