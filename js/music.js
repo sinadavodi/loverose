a5d4dcd4f8708d4f9a8591e5800d4c217cf6a708
sinadavodi <117124411+sinadavodi@users.noreply.github.com>
2026-01-31 04:47:32 +0330

    Update initMusic to handle volume and slider
    Refactor initMusic function to use volume from State and add volume slider functionality.

--- js/music.js
+++ js/music.js
@@ -1,9 +1,14 @@
-export function initMusic(Personality){
-  const audio = new Audio(`https://dl.musicsweb.ir/musics/04/08/Younes%20Bayat%20-%20Gomam%20Kon%20-%20320.mp3`);
-  audio.loop = true;
-  audio.volume = 0.5;
 
-  document.body.addEventListener("click",()=>{
-    audio.play().catch(()=>{});
-  },{ once:true });
+export function initMusic(Person, State) {
+  const audio = new Audio(`https://dl.musicsweb.ir/musics/04/08/Younes%20Bayat%20-%20Gomam%20Kon%20-%20320.mp3`);
+  audio.volume = State.volume/100;
+  audio.loop = true;
+
+  document.getElementById("volumeSlider")
+    .addEventListener("input", e => {
+      audio.volume = e.target.value/100;
+      localStorage.volume = e.target.value;
+    });
+
+  document.body.addEventListener("click", ()=> audio.play(), {once:true});
 }
