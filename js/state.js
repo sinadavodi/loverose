636f36c13580dfb3fddffc786eaf3030ce49ab72
sinadavodi <117124411+sinadavodi@users.noreply.github.com>
2026-01-31 04:40:17 +0330

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,5 +1,8 @@
 export const State = {
-  awake: false,
-  attention: 100,
-  lastVisit: Date.now()
+  startDay: localStorage.startDay || (localStorage.startDay = Date.now()),
+  lastDisplay: localStorage.lastDisplay || "",
+  volume: localStorage.volume || 50
 };
+
+State.daysTogether =
+  Math.floor((Date.now() - State.startDay) / (1000*60*60*24));
