7be309a88e7f8900d5f232de9e43d493d70c61ab
sinadavodi <117124411+sinadavodi@users.noreply.github.com>
2026-01-31 05:20:23 +0330

    Refactor imports and update initScene parameters

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
-
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.158/build/three.module.js";
 import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.158/examples/jsm/loaders/GLTFLoader.js";
+import { RGBELoader } from "https://cdn.jsdelivr.net/npm/three@0.158/examples/jsm/loaders/RGBELoader.js";
 
 import { State } from "./state.js";
-import { getMood, Personality } from "./personality.js";
 import { initScene } from "./scene.js";
+
+initScene(THREE, GLTFLoader, RGBELoader, State, Personality[moodKey]);
+
+import { getMood, Personality } from "./personality.js";
 import { initMusic } from "./music.js";
 import { initUI } from "./ui.js";
 
 const moodKey = getMood(State.daysTogether);
-initScene(THREE, GLTFLoader, Personality[moodKey]);
 initMusic(Personality[moodKey], State);
 initUI(State, Personality[moodKey]);
 
 if (new Date().getHours() >= 22) 
   document.getElementById("nightStyle").disabled = false;
+
