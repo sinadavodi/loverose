944d2a3bafa7a6fcc902c7eae10ed4e1db8e9211
sinadavodi <117124411+sinadavodi@users.noreply.github.com>
2026-01-31 04:08:28 +0330

    Change music source to a new audio file

--- js/music.js
+++ js/music.js
@@ -1,5 +1,5 @@
 export function initMusic(Personality){
-  const audio = new Audio(`/assets/music/HaminBood.mpeg`);
+  const audio = new Audio(`https://dl.musicsweb.ir/musics/04/08/Younes%20Bayat%20-%20Gomam%20Kon%20-%20320.mp3`);
   audio.loop = true;
   audio.volume = 0.5;
 
